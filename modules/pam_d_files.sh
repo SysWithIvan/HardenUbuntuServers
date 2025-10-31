c2c37bf075125a205756e6b73e8b1e071125f721
IvanTexenery <51971959+SysWithIvan@users.noreply.github.com>
2025-10-31 07:57:38 +0000

    Update pam_d_files.sh

--- modules/pam_d_files.sh
+++ modules/pam_d_files.sh
@@ -12,14 +12,14 @@
 # (at your option) any later version. See the LICENSE file for details.       
 # =======================================================================
 
-grep -E '^\s*auth\s*required\s*pam_wheel.so\s*use_uid\s*group\s*=\s*.*'  /etc/pam.d/su && \
-sed -i 's/^\s*auth\s*required\s*pam_wheel.so\s*use_uid\s*group\s*=\s*.*/auth required pam_wheel.so use_uid group=admins/'\
-/etc/pam.d/su || echo 'auth required pam_wheel.so use_uid group=admins' >> /etc/pam.d/su
+grep -Eq '^[[:space:]]*auth[[:space:]]*required[[:space:]]*pam_wheel\.so[[:space:]]*use_uid[[:space:]]*group[[:space:]]*=[[:space:]]*.+$' /etc/pam.d/su && \
+sed -E -i 's/^[[:space:]]*auth[[:space:]]*required[[:space:]]*pam_wheel\.so[[:space:]]*use_uid[[:space:]]*group[[:space:]]*=[[:space:]]*.+$/auth required pam_wheel.so use_uid group=admins/' /etc/pam.d/su || \
+echo 'auth required pam_wheel.so use_uid group=admins' >> /etc/pam.d/su
 
 cat >/etc/pam.d/common-password <<'EOF'
 # here are the per-package modules (the "Primary" block)
 password        requisite                       pam_pwquality.so try_first_pass retry=3 minlen=8 minclass=3 difok=3
-password        requisite                       pam_pwhistory.so remember=7 use_authtok
+password        requisite                       pam_pwhistory.so remember=24 use_authtok enforce_for_root
 password        [success=3 default=ignore]      pam_krb5.so minimum_uid=5000
 password        [success=2 default=ignore]      pam_unix.so obscure use_authtok try_first_pass yescrypt
 password        [success=1 user_unknown=ignore default=die]     pam_ldap.so use_authtok try_first_pass
