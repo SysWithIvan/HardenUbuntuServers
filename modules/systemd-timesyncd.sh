3f129fe255cf8b4d5afb045154b4baa7251cc594
IvanTexenery <51971959+SysWithIvan@users.noreply.github.com>
2025-11-17 07:58:37 +0000

    Update NTP and FallbackNTP settings in config

--- modules/systemd-timesyncd.sh
+++ modules/systemd-timesyncd.sh
@@ -14,9 +14,9 @@
 
 systemctl unmask systemd-timesyncd.service
 systemctl --now enable systemd-timesyncd.service
-grep -E '^[#]*NTP[[:space:]]*=' /etc/systemd/timesyncd.conf && sed -i 's/^[#]*NTP[[:space:]]*=/NTP=hora.roa.es/' /etc/systemd/timesyncd.conf \
+grep -E '^[#]*NTP[[:space:]]*=' /etc/systemd/timesyncd.conf && sed -i 's/^[#]*NTP[[:space:]]*=.*/NTP=hora.roa.es/' /etc/systemd/timesyncd.conf \
 || echo 'NTP=hora.roa.es' >> /etc/systemd/timesyncd.conf
 
 grep -E '^[#]*FallbackNTP[[:space:]]*=' /etc/systemd/timesyncd.conf \
-&& sed -i 's/^[#]*FallbackNTP[[:space:]]*=/FallbackNTP=ntp.ubuntu.com/' /etc/systemd/timesyncd.conf \
+&& sed -i 's/^[#]*FallbackNTP[[:space:]]*=.*/FallbackNTP=ntp.ubuntu.com/' /etc/systemd/timesyncd.conf \
 || echo 'NTP=ntp.ubuntu.com' >> /etc/systemd/timesyncd.conf
